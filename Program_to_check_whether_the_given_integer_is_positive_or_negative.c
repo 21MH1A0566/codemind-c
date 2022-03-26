a5445aee106eec2abf654f74eaa927244d0dc7ee
21MH1A0566 <98518414+21MH1A0566@users.noreply.github.com>
2022-03-26 12:24:57 +0530

    #include<stdio.h> int main() {     int N;     scanf("%d",&N);     if(N>0)     {       printf("Positive Number");
        }
        else
    {
        printf("Negative Number");
        
    }
    return 0;
    }

---
+++ Program_to_check_whether_the_given_integer_is_positive_or_negative.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    if(N>0)
+    {
+      printf("Positive Number");
+      
+    }
+    else
+{
+    printf("Negative Number");
+    
+}
+return 0;
+}
